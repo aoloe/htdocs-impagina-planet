a9ecf03af5c51acbd1c597aeb5a757f6a75f860b
Manuel Schmalstieg <git.code@ms-studio.net>
2013-02-01 21:46:35 +0100

    basic PREV/NEXT browsing via the left/right arrow keys

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,8 @@ $(".open-button").show();
 
 // prepare some markup
 $(document.body).append("<div id='reader' class='reader hidden'><div class='reader-inside'></div><div class='reader-bg close-button'></div></div><div id='mask' class='mask-layer hidden'></div>");
+ 				
+}); // end document ready
 
 
 /*  
@@ -35,26 +37,41 @@ $(document.body).append("<div id='reader' class='reader hidden'><div class='read
   };
 })( jQuery );
 
+
+(function( $ ){
+  $.fn.loadNextItem = function() {
+  			// clone the next/previous item
+  			var clonage = $("#planet-item-"+window.item_nr+" .item-inside").clone(true);
+  		  $('#reader .reader-inside').html(clonage);
+  		  $('#reader .post-content').css({'height': 'auto','max-height': window.new_container_height+'px'});
+  };
+})( jQuery );
+
+
 /*  
  4. What happens when we open the reader
 */
 
 $(".open-button").on("click", function() {
 
-	var clonage = $(this).parent().clone(true);
-	
-	var item_height = $(this).parent().height();
-	var new_container_height = ($(window).height()) - 260;
-			    
-	$('#reader').fadeIn(300);
-	$('#mask').fadeIn(300);
-	
-	$('#reader .reader-inside').html(clonage);
-	
-	$('#reader .post-content').css({'height': 'auto','max-height': new_container_height+'px'});
-		    
-	return false;
+				var clonage = $(this).parent().clone(true);
+				
+				window.item_nr = $(this).parent().parent('.item').attr('id').slice(12);
+				
+				var item_height = $(this).parent().height();
+				window.new_container_height = ($(window).height()) - 260;
+						    
+				$('#reader').fadeIn(300);
+				$('#mask').fadeIn(300);
+				
+				$('#reader .reader-inside').html(clonage);
+				
+				$('#reader .post-content').css({'height': 'auto','max-height': new_container_height+'px'});
+				
+				// alert("item is: " + window.item_nr);
+				return false;
 });
+
 
 /*  
  5. What happens when we close the reader
@@ -71,9 +88,38 @@ $(document).keyup(function(e) {
   }
 });
 
+/*  
+ 6. What happens when we click on the LEFT or RIGHT arrow keys
+*/
+
+$(document).keydown(function(e){
+    if (e.keyCode == 37) { 
+    		// LOAD content of prev item.
+    		if (window.item_nr == 1) {
+    			$(document).closeReader();
+    		} else if (window.item_nr) {
+	       	window.item_nr = parseInt(window.item_nr) - 1;
+	       	$(document).loadNextItem();
+	      }
+    }
+});
+
+/*  
+ 7. What happens when we click on the LEFT or RIGHT arrow keys
+*/
+
+$(document).keydown(function(e){
+    if (e.keyCode == 39) { 
+				// LOAD content of next item.
+				if (window.item_nr) {
+				    window.item_nr = parseInt(window.item_nr) + 1;
+				    $(document).loadNextItem();
+				} 
+    }
+});
+
 /* 
  * that's it - thanks for reading !
  ****************************************************
  */
- 				
-}); // end document ready
+
